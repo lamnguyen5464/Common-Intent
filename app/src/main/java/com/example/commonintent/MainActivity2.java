f88bbd5e738aa91562dfbb862a847fd53321758f
Lam Nguyen <lamnguyen5464@Lams-MacBook-Pro.local>
2021-06-01 18:06:51 +0700

    [intent-contact] initialize

---
+++ app/src/main/java/com/example/commonintent/MainActivity2.java
@@ -0,0 +1,54 @@
+package com.example.commonintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.commonintent.helpers.ContactHelper;
+
+public class MainActivity2 extends AppCompatActivity implements View.OnClickListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2_main);
+
+        Button btSelectContact = findViewById(R.id.btSelectConact);
+        btSelectContact.setOnClickListener(this);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK){
+            return;
+        }
+        switch (requestCode){
+            case ContactHelper.REQUEST_SELECT_CONTACT:
+                ContactHelper.handleOnReturn(this, data);
+                break;
+            default:
+        }
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btSelectConact:
+                ContactHelper.selectContact(this);
+                break;
+            default:
+                Toast.makeText(this, "This button has not been handled", Toast.LENGTH_SHORT);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/commonintent/helpers/ContactHelper.java
@@ -0,0 +1,35 @@
+package com.example.commonintent.helpers;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.widget.Toast;
+
+import com.example.commonintent.constant.IntentTypes;
+
+public class ContactHelper {
+    public static final int REQUEST_SELECT_CONTACT = IntentTypes.REQUEST_SELECT_CONTACT;
+
+    public static void selectContact(Activity activity) {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+        if (intent.resolveActivity(activity.getPackageManager()) != null) {
+            activity.startActivityForResult(intent, REQUEST_SELECT_CONTACT);
+        }
+    }
+
+    public static void handleOnReturn(Activity activity, Intent data) {
+        Uri contactUri = data.getData();
+        String[] projection = new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER};
+        Cursor cursor = activity.getContentResolver().query(contactUri, projection,
+                null, null, null);
+        // If the cursor returned is valid, get the phone number
+        if (cursor != null && cursor.moveToFirst()) {
+            int numberIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+            String number = cursor.getString(numberIndex);
+            Toast.makeText(activity, "You chose phone number: " + number, Toast.LENGTH_SHORT);
+        }
+    }
+}
